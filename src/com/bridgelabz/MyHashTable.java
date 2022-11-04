5a2bd5c8bfed754efde4ee87f2a520d83c790d2b
NagrajPanchal <nagrajsubhashpanchal@gmail.com>
2022-11-04 09:59:05 +0530

    Added : Frequency of words

--- src/com/bridgelabz/MyHashTable.java
+++ src/com/bridgelabz/MyHashTable.java
@@ -1,15 +1,68 @@
 package com.bridgelabz;
-
-public class MyHashTable
+import java.util.Hashtable;
+import java.util.ArrayList;
+import java.util.List;
+public class MyHashTable<K,V>
 {
-        public static void main(String[] args) {
+    List<MyLinkedList> myBucketArray;
+    int bucketSize;
 
-            String paragraph = "Paranoids are not paranoid because they are paranoid but " +
-                    "because they keep putting themselves deliberately into paranoid avoidable situations";
+    public MyHashTable() {
+    }
+    public MyHashTable(int bucketSize) {
+        this.bucketSize = bucketSize;
+        myBucketArray = new ArrayList<>(bucketSize);
+        for (int i = 0; i < 11; i++) {
+            myBucketArray.add(i, null);
+        }
+    }
 
-            String[] words = paragraph.toLowerCase().split(" ");
-            MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
-            System.out.println(words);
+    public int getIndex(K key) {
+        int index = Math.abs(key.hashCode() % bucketSize);
+        return index;
+    }
+
+    public void add(K key, V value) {
+        int indexValue = getIndex(key);
+        MyLinkedList<K> myLinkedList1 = myBucketArray.get(indexValue);
+        if (myLinkedList1 == null) {
+            MyLinkedList<K> myLinkedList = new MyLinkedList<>();
+            INode<K> newNode = new MyMapNode<>(key, value);
+            myLinkedList.add(newNode);
+            myBucketArray.set(indexValue, myLinkedList);
+        }
+        else {
+            MyMapNode<K,V> searchNode = (MyMapNode<K, V>) myLinkedList1.search(key);
+            if(searchNode != null){
+                searchNode.setValue(value);
+            }
+            else {
+                INode<K> newNode = new MyMapNode<>(key,value);
+                myLinkedList1.add(newNode);
+            }
         }
 
+    }
+
+    public V findFrequency(K key){
+        int indexValue = getIndex(key);
+        MyLinkedList<K> myLinkedList1 = myBucketArray.get(indexValue);
+        if (myLinkedList1==null){
+            return  null;
+        }else {
+            MyMapNode<K,V> searchNode = (MyMapNode<K, V>) myLinkedList1.search(key);
+            if (searchNode==null){
+                return null;
+            }else {
+                return searchNode.getValue();
+            }
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        return  ""+ myBucketArray ;
+
+    }
 }
